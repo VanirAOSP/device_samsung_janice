9863bc665e1652b158667009cc3b75019be053c3
C457 <android.c357@gmail.com>
2014-06-27 14:49:14 -0500

    janice:Remove releasetools
    This is done in u8500-common
    
    Change-Id: I277e74200f9605989b5802fa3e3710bc27d5895e

--- releasetools.py
+++
@@ -1,3 +0,0 @@
-def FullOTA_InstallEnd(info):
-	info.script.AppendExtra('symlink("/system/lib/libjhead.so", "/system/lib/libhead.so");')
-	info.script.AppendExtra('run_program("/sbin/make_ext4fs", "/dev/block/mmcblk0p9");')
